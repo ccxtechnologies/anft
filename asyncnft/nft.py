75322f01556d58d498b14663511fad17b0e0e3cb
Charles Eidsness <charles@ccxtechnologies.com>
2018-07-28 08:40:01 -0400

    begrudgingly add delay after add because nft process returns before complete

--- asyncnft/nft.py
+++ asyncnft/nft.py
@@ -23,4 +23,11 @@ async def nft(*command):
                     f"\n{stderr.decode()}"
             )
 
+    # I hate having to do this but the nft process
+    # will sometimes complete and add before the
+    # add is complete in the kernel, so if you
+    # try to operate on the new item it will fail.
+    if command[0] == 'add':
+        await asyncio.sleep(0.5)
+
     return stdout.decode()
